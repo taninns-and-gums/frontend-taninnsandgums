4134a287b336e8e6e6065f5491cb063f07ab40dd
giancarlo sinuiri <gsinuiri@gmail.com>
2026-02-22 12:56:02 -0500

    22-02 1310

--- src/studio-tannins-and-gums/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import {localeString} from './objects/localeString'
-import {localeText} from './objects/localeText'
-import {product} from './product'
-
-export const schemaTypes = [product, localeString, localeText]
